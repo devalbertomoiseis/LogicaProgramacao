65bc1939bfc146f7de16985bfc47ca229c77c15d
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-08-27 16:59:23 -0300

    Trabalhando com tipos de variaveis

---
+++ src/VariaveisConstantes/TiposDeDados02.java
@@ -0,0 +1,26 @@
+package VariaveisConstantes;
+
+public class TiposDeDados02 {
+    public static void main(String[] args) {
+        /*
+        Tipos:
+            - Numérico;
+            - Texto;
+            - Lógico
+        Numéricos:
+            - Byte;
+            - Short;
+            - Integer;
+            - Long;
+        Numéricos (Decimais):
+            - Float;
+            - Double;
+        Texto:
+            - Char;
+            - String;
+        Lógico:
+        - Boolean; Usado para tomar decisões;
+        */
+
+    }
+}
---
+++ src/VariaveisConstantes/TiposPrimitivos03.java
@@ -0,0 +1,7 @@
+package VariaveisConstantes;
+
+public class TiposPrimitivos03 {
+    public static void main(String[] args) {
+
+    }
+}
