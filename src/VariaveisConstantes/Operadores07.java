aa24671411bae65bf62ceea7d9ab74b1e906730d
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-08-28 17:56:27 -0300

    Trabalhando com operadores

---
+++ src/VariaveisConstantes/Operadores07.java
@@ -0,0 +1,20 @@
+package VariaveisConstantes;
+
+public class Operadores07 {
+    public static void main(String[] args) {
+        /*
+            + e - -> adição e subtração
+            * e / -> multiplicação e divisão
+            % -> módulo
+            > e >= -> maior que e maior ou igual a
+            < e <= ->  menor que e menor ou igual a
+            == e !== ->  igual e diferente
+            = simplesmente, atribui algum valor para variavel
+            += e -= -> faz a operação - adição ou subtração e, depois, a atribuição
+            *= e /= -> faz a operação - multiplicação ou divisão e, depois, a atribuição
+            %= faz a operação, módulo e, depois, a atribuição
+            && os dois lados deve ser verdadeiro
+            ! reverte o valor
+        */
+    }
+}
